1bcfe1301573d83f5788c87bd6f71cc9ef8d4ed9
NidhiSingh-25 <nidhi630694@gmail.com>
2025-09-18 23:44:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function firstWord(s) {
   // your code here
+	 if (!str) return ""; // handle empty string
+
+  str = str.trim(); // remove leading/trailing spaces
+
+  let spaceIndex = str.indexOf(" ");
+
+  if (spaceIndex === -1) {
+    return str; // no space found, return entire string
+  }
+
+  return str.substring(0, spaceIndex); // return part before space
+
 }
 
 // Do not change the code below
